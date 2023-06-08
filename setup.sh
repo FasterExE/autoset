04bd4918019a05fb8589708ed7663efe5f2f626c
Gilang Tok'yo <123780158+givpn@users.noreply.github.com>
2023-06-08 07:16:38 +0700

    x

--- setup.sh
+++ setup.sh
@@ -10,6 +10,9 @@ if [ $MYIP = $IZIN ]; then
 echo -e "\e[32mPermission Accepted...\e[0m"
 else
 echo -e "\e[31mPermission Denied!\e[0m";
+echo -e "\e[32mContact : t.me/givpn\e[0m";
+echo -e "\e[32mFor Register IP\e[0m";
+sleep 60
 exit 0
 fi
 # System
