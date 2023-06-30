f3d61212c3e260b9a50dd17a624987640aceeb9b
Gilang Tok'yo <123780158+givpn@users.noreply.github.com>
2023-06-30 07:16:11 +0700

    x

---
+++ sslh-fix/sslh-fix-reboot.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+# ==========================================
+
+sleep 1
+systemctl stop ws-tls 
+pkill python
+systemctl disable --now haproxy
+systemctl start haproxy
+systemctl enable haproxy
+sudo systemctl restart haproxy
+systemctl daemon-reload
+systemctl disable ws-tls
+systemctl daemon-reload
+systemctl enable ws-tls
+systemctl start ws-tls
+systemctl restart ws-tls
+sleep 1
+restart
